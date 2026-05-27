85bcabdf52746db7917c7c2427a3d437b1030f9c
Lunga-Sityebi <01lungasityebi>
2026-05-27 21:48:32 +0200

    feat: update forgot and reset password forms to use JavaScript functions for handling submissions

--- src/view/scripts/forgotPassword.js
+++ src/view/scripts/forgotPassword.js
@@ -1,11 +1,9 @@
 const form = document.getElementById('forgotForm');
 
-form.addEventListener('submit', async (e) => {
-
-    e.preventDefault();
-
+async function sendResetLink() {
     const email =
         document.getElementById('email').value;
+        console.log(email)
 
     const res = await fetch(
         'http://localhost:3000/api/auth/forgot-password',
@@ -21,4 +19,28 @@ form.addEventListener('submit', async (e) => {
     const data = await res.json();
 
     alert(data.message);
-});
+}
+
+// form.addEventListener('submit', async (e) => {
+
+//     e.preventDefault();
+
+//     const email =
+//         document.getElementById('email').value;
+//         console.log(email)
+
+//     const res = await fetch(
+//         'http://localhost:3000/api/auth/forgot-password',
+//         {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ email })
+//         }
+//     );
+
+//     const data = await res.json();
+
+//     alert(data.message);
+// });
--- src/view/scripts/resetPassword.js
+++ src/view/scripts/resetPassword.js
@@ -2,14 +2,9 @@ const params = new URLSearchParams(window.location.search);
 
 const token = params.get('token');
 
-document
-.getElementById('resetForm')
-.addEventListener('submit', async (e) => {
-
-    e.preventDefault();
-
-    const password =
-        document.getElementById('password').value;
+async function resetPassword() {
+    const password = document.getElementById('password').value;
+    console.log(password);
 
     const res = await fetch(
         'http://localhost:3000/api/auth/reset-password',
@@ -29,7 +24,42 @@ document
 
     alert(data.message);
 
+    alert(data.message);
+
     if (res.ok) {
         window.location.href = 'login.html';
     }
-});
+}
+
+
+// document
+// .getElementById('resetForm')
+// .addEventListener('submit', async (e) => {
+
+//     e.preventDefault();
+
+//     const password =
+//         document.getElementById('password').value;
+
+//     const res = await fetch(
+//         'http://localhost:3000/api/auth/reset-password',
+//         {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 token,
+//                 password
+//             })
+//         }
+//     );
+
+//     const data = await res.json();
+
+//     alert(data.message);
+
+//     if (res.ok) {
+//         window.location.href = 'login.html';
+//     }
+// });
