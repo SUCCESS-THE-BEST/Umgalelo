e3add3111c291fde412e62e5fc241efb2a453e84
Lunga-Sityebi <01lungasityebi>
2026-05-27 19:50:13 +0200

    feat: update CORS configuration to include additional origins for socket connections

--- server.js
+++ server.js
@@ -11,13 +11,15 @@ const server = http.createServer(app);
 /* SOCKET.IO */
 
 const io = new Server(server, {
-    cors: {
-        origin: [
-            'http://127.0.0.1:5501',
-            'http://localhost:5501'
-        ],
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost:5500',
+      'http://127.0.0.1:5501',
+      'http://localhost:5501'
+    ],
+    methods: ['GET', 'POST']
+  }
 });
 
 /* SOCKET CONNECTION */
--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const path = require('path')
 
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5501', 'http://localhost:5501'],
+  origin: [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+    'http://127.0.0.1:5501',
+    'http://localhost:5501'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
